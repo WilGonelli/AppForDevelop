b40d30aed2d926a2ab61dd31530d087abff8743a
William Vieira <William.Vieira@fit-tecnologia.org.br>
2023-06-14 15:46:23 -0300

    created project of hangman game

---
+++ HangmanGame/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {View, Text,TextInput} from 'react-native';
+import styles from "./src/assets/Styles";
+
+
+export default function App(){
+  
+  return(
+    <View style={styles.container}>
+      <Text>Hello World</Text>
+    </View>
+  )
+};
